2b7175a2084d9204244992f10aac83b58077db0c
Mahdi Haroun <148803240+MahdiHaroun@users.noreply.github.com>
2023-11-13 20:31:01 +0300

    Update Week 2.py

--- Week 2.py
+++ Week 2.py
@@ -1,49 +1,49 @@
 
-"""import random 
+import random 
 prob = random.random() # void magnitude between 0 and 1 
 print(prob)
 diceThrow = random.randrange(1,7) # return a integer one of 1,2,3,4,5,6,7
 print(diceThrow)
-#randrange.random is not allowed """
-"""#another method 
+#randrange.random is not allowed 
+#another method 
 from random import randrange , random 
 prob=random()
 print (prob)
 diceThrow = randrange()
-print(diceThrow)"""
+print(diceThrow)
 
-"""digets = [1,2,3,4]
+digets = [1,2,3,4]
 for _ in digets :
     print(_)
 else: 
-    print("nothing left to print")"""
-"""x =10 
+    print("nothing left to print")
+x =10 
 while ( x < 20):
     print("hello")
-    x +=1 """
-"""myTuble = (4,2,4)
+    x +=1 
+myTuble = (4,2,4)
 myTuble2= (4,)   #without the comma it is a int 
-"""
-"""s = "python"
+
+s = "python"
 mylist = [ 1 , 2,3]
 print(mylist[2])
 print (s [0])
 print (len(s))
-print(len(mylist))"""
-"""julia = ( " Julia" , "Roberts" , 1967 , "Dublicty", 2009, "Actress" , "atlanta georgia")
+print(len(mylist))
+julia = ( " Julia" , "Roberts" , 1967 , "Dublicty", 2009, "Actress" , "atlanta georgia")
 print(julia[2])
 print(julia[2:6]) # from 2 to 6 
 print (len(julia))
 julia = julia[:3] + (" Eat pray love", 2010)+ julia[5:]
-print(julia)"""
-""""
+print(julia)
+
 #creating a list with concatenation and repetition
 fruit= [ "apple"] 
 print([1,2] + [3,4])
 print (fruit + [1,2,3])
 print (([0,1] + fruit) *4)
-"""
-"""a = "I have had an apple on my desk before !"
+
+a = "I have had an apple on my desk before !"
 print(a.count("e"))  #5
 print(a.count("ha")) #2
 z= [ "atoms" , 4, ' neutron' , 6 , "ptoton" ,4 , "electron" , 4, "electron" ]
@@ -58,7 +58,7 @@ print(music.index("your")) #9
 print(bio.index("Mahdi")) #0
 print  (bio.index([])) #3
 print (bio.index(43)) #6 """
-"""song = "the rain in spain..."
+song = "the rain in spain..."
 wds = song.split()
 print (wds)
 suii= "leaders and best"
@@ -70,28 +70,28 @@ s=glue.join(was)
 print(was)
 print("***".join(was))
 print("".join(was))
-"""
+
 """ ***************************************************************************************************"""
 """the for loop"""
-"""for suii in ["joe" , "A" , "b" , "c" ] :
+for suii in ["joe" , "A" , "b" , "c" ] :
     print("hi", suii,  "please come to my party")
 
 
 for achar in "Go spot GO"  :
     print (achar)  
-"""
+
 
 #how many hellos will be printed """
-"""s= "python rocks"
+s= "python rocks"
 for cs in s : 
     print("Hello")  #12 
-"""
-"""s= "python rocks"
+
+s= "python rocks"
 for cs in s [3:8]: 
     print("Hello")   #5 
-"""
 
-"""
+
+
 import turtle 
 wn=turtle.Screen()
 alex=turtle.Turtle()
@@ -99,15 +99,15 @@ for aColor in ["yellow","red","purple","blue"] : #not important what inside the 
         alex.color(aColor)
         alex.forward(50)
         alex.left(90)
-"""
-"""
+
+
 nums= [1,2,3,4,5,6,7,8,9,10]
 accum=0
 for w in nums :
     accum = accum + w 
 print(accum) 
-"""
-"""nums= [1,2,3,4,5,6,7,8,9,10]
+
+nums= [1,2,3,4,5,6,7,8,9,10]
 print("range(5):")
 for i in range(5) :
     print (i)
@@ -116,16 +116,11 @@ for i in range (1,5):
     print (i)
 
 print(list(range(5)))
-print(list(range(0,5)))"""
+print(list(range(0,5)))
 
 # for sequnces give a plural noun , fr itteration variable give a singular name . 
 
-print(True)
-print(False)
-print(type(True))
-print(type(False))
-print(5==5)
-print(5==6) 
+
 
 
     
